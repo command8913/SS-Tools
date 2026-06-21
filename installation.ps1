eb68922118665c42fa9e2a5d264dfb7c5887492f
command8913 <165208607+command8913@users.noreply.github.com>
2026-06-21 14:17:05 +0300

    Add files via upload
    Signed-off-by: command8913 <165208607+command8913@users.noreply.github.com>

---
+++ installation.ps1
@@ -0,0 +1,19 @@
+$installPath = "C:\ss"
+if (!(Test-Path $installPath)) {
+    New-Item -ItemType Directory -Path $installPath -Force
+}
+
+$files = @(
+    @{Name="Everything-Setup.exe"; Url="https://www.voidtools.com/Everything-1.5.0.1415b.x64-Setup.exe"},
+    @{Name="JournalTraceNormal.exe"; Url="https://github.com/spokwn/JournalTrace/releases/download/1.2/JournalTraceNormal.exe"},
+    @{Name="pv+.exe"; Url="https://github.com/command8913/SS-Tools/raw/main/pv%2B.exe"}
+)
+
+foreach ($file in $files) {
+    $output = Join-Path $installPath $file.Name
+    (New-Object System.Net.WebClient).DownloadFile($file.Url, $output)
+}
+
+Get-ChildItem -Path $installPath -Filter "*.exe" | ForEach-Object {
+    Start-Process -FilePath $_.FullName
+}
