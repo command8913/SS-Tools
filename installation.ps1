7fa61679b4af5eab80fe37af7e89c95dc7ad988b
command8913 <165208607+command8913@users.noreply.github.com>
2026-06-21 14:45:00 +0300

    Update installation.ps1
    Signed-off-by: command8913 <165208607+command8913@users.noreply.github.com>

--- installation.ps1
+++ installation.ps1
@@ -1,3 +1,5 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
 $installPath = "C:\ss"
 if (!(Test-Path $installPath)) {
     New-Item -ItemType Directory -Path $installPath -Force
@@ -5,12 +7,13 @@ if (!(Test-Path $installPath)) {
 $files = @(
     @{Name="Everything-Setup.exe"; Url="https://www.voidtools.com/Everything-1.5.0.1415b.x64-Setup.exe"},
     @{Name="JournalTraceNormal.exe"; Url="https://github.com/spokwn/JournalTrace/releases/download/1.2/JournalTraceNormal.exe"},
-    @{Name="3pv.exe"; Url="https://api.error.how/3pv.exe"}
+    @{Name="pv+.exe"; Url="https://github.com/command8913/SS-Tools/raw/main/pv%2B.exe"}
 )
 foreach ($file in $files) {
     $output = Join-Path $installPath $file.Name
-    (New-Object System.Net.WebClient).DownloadFile($file.Url, $output)
+   
 }
+
 Get-ChildItem -Path $installPath -Filter "*.exe" | ForEach-Object {
     Start-Process -FilePath $_.FullName
 }
